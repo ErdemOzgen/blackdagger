7951418bd7f4987d9d36b86f7d19c6bd9ad36857
odd509 <gozlu.oguz@gmail.com>
2025-04-24 17:27:26 +0300

    feat(fe/dag graphs): improve graph styling by better status color coding and animated links and display preconditions of steps on top of the links, update mermaid version

--- ui/src/components/molecules/Graph.tsx
+++ ui/src/components/molecules/Graph.tsx
@@ -38,6 +38,7 @@ function Graph({
     backgroundSize: '20px 20px',
     border: '2px solid ',
   };
+  var edgeId = 0;
   const graph = React.useMemo(() => {
     if (!steps) {
       return '';
@@ -50,30 +51,82 @@ function Graph({
       const id = step.Name.replace(/\s/g, '_');
       const c = graphStatusMap[status] || '';
       dat.push(`${id}[${step.Name}]${c};`);
+
+      const labelLines = step.Preconditions?.length
+        ? step.Preconditions.map((c) => `${c.Condition} = ${c.Expected}`).join(
+            '<br/>'
+          )
+        : '';
+
       if (step.Depends) {
         step.Depends.forEach((d) => {
           const depId = d.replace(/\s/g, '_');
-          dat.push(`${depId} --> ${id};`);
+
+          const linkOp =
+            status === NodeStatus.Error ||
+            status === NodeStatus.Cancel ||
+            status === NodeStatus.Skipped
+              ? '-.-x' // Dotted crossing for Error, Cancel, or Skipped
+              : '-->'; // Default normal arrow (fallback for other statuses)
+          const edgeIdStr = `e${edgeId}@`;
+          const edgeStr = labelLines
+            ? `${depId} ${edgeIdStr}${linkOp} |"${labelLines}"| ${id};`
+            : `${depId} ${edgeIdStr}${linkOp} ${id};`;
+
+          dat.push(edgeStr);
+
+          if (status === NodeStatus.Running || status === NodeStatus.Success) {
+            // green solid line
+            dat.push(
+              `linkStyle ${edgeId} stroke:lime,stroke-width:3px,color:black,background-color:black`
+            );
+            if (status === NodeStatus.Running) {
+              dat.push(`${edgeIdStr}{ animate: true }`);
+            }
+          } else if (
+            status === NodeStatus.Error ||
+            status === NodeStatus.Cancel ||
+            status === NodeStatus.Skipped
+          ) {
+            // red broken line
+            dat.push(
+              `linkStyle ${edgeId} stroke:red,stroke-width:3px,color:red`
+            );
+          }
         });
+        edgeId++;
       }
-      if (onClickNode) {
-        dat.push(`click ${id} onClickMermaidNode`);
-      }
+
+      if (onClickNode) dat.push(`click ${id} onClickMermaidNode`);
     };
+
     if (type == 'status') {
       (steps as Node[]).forEach((s) => addNodeFn(s.Step, s.Status));
     } else {
       (steps as Step[]).forEach((s) => addNodeFn(s, NodeStatus.None));
     }
+
     dat.push(
-      'linkStyle default stroke:#999,stroke-width:3px,fill:none,color:#333'
+      'linkStyle default stroke:#999,stroke-width:3px,fill:none,color:#333, '
     );
-    dat.push('classDef none color:#333,fill:white,stroke:lightblue,stroke-width:3px');
-    dat.push('classDef running color:#333,fill:white,stroke:lime,stroke-width:3px');
-    dat.push('classDef error color:#333,fill:white,stroke:red,stroke-width:3px');
-    dat.push('classDef cancel color:#333,fill:white,stroke:pink,stroke-width:3px');
-    dat.push('classDef done color:#333,fill:white,stroke:green,stroke-width:3px');
-    dat.push('classDef skipped color:#333,fill:white,stroke:gray,stroke-width:3px');
+    dat.push(
+      'classDef none color:#333,fill:white,stroke:lightblue,stroke-width:3px'
+    );
+    dat.push(
+      'classDef running color:#333,fill:white,stroke:#2563eb,stroke-width:3px'
+    );
+    dat.push(
+      'classDef error color:#333,fill:white,stroke:red,stroke-width:3px'
+    );
+    dat.push(
+      'classDef cancel color:#333,fill:white,stroke:pink,stroke-width:3px'
+    );
+    dat.push(
+      'classDef done color:#333,fill:white,stroke:lime,stroke-width:5px'
+    );
+    dat.push(
+      'classDef skipped color:#333,fill:white,stroke:gray,stroke-width:3px'
+    );
     return dat.join('\n');
   }, [steps, onClickNode, flowchart]);
   return <Mermaid style={mermaidStyle} def={graph} />;
